e46eb3af56d72411d165b48b4c6de1b81ae4f1ac
Cho0 <choojason7@gmail.com>
2021-09-06 17:03:51 -0700

    2.5 task
    Base requests and response

--- index.js
+++ index.js
@@ -18,8 +18,45 @@ app.use(express.json());
 app.use(methodOverride());
 
 app.get('/movies', (req, res) => {
-  res.send(topMovies);
+  res.send('Successful GET request returning data on all movies');
 });
+
+app.get('/movieinfo', (req,res) => {
+  res.send('Successful Get request retuning data on movie information');
+});
+
+app.get('/movies/genres', (req, res) => {
+  res.send('Successful GET request returning list of all genres');
+});
+
+app.get('/movieinfo/director', (req, res) => {
+  res.send('Successful Get request returning movie director information');
+});
+
+app.post('/register', (req, res) => {
+  res.send('Successful POST request in creation of new user');
+});
+
+app.put('/update', (req, res) => {
+  res.send('Successful PUT request in updating user info');
+});
+
+app.get('/favorites', (req, res) => {
+  res.send('Successful GET request in returning data on user favorite movies');
+});
+
+app.put('/favorites/add', (req, res) => {
+  res.send('Successful PUT request in adding movie into user favorites list');
+});
+
+app.delete('/favorites/remove', (req, res) => {
+  res.send('Successful DELETE request in removing movie from user favorites list');
+});
+
+app.delete('/deregister', (req, res) => {
+  res.send('Successful DELETE request in removing account from external source')
+})
+
 
 app.get('/documentation', (req, res) =>{
   res.sendFile(__dirname + '/public/documentation.html');
