b0780aa4dc0b552a7f19e5ca55a9ff353c723b99
Cho0 <choojason7@gmail.com>
2021-09-23 17:07:12 -0700

    HELP ME BEN
    AAAAAA IM HELLA BEHIND BRO

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const fs = require('fs');
 const topMovies = require('./movie_list');
+const modules = require('./models');
+
+
 
 const app = express();
+
 
 app.use(morgan('common', {
   stream: fs.createWriteStream('./log.txt', {flags: 'a'})
@@ -18,7 +22,14 @@ app.use(express.json());
 app.use(methodOverride());
 
 app.get('/movies', (req, res) => {
-  res.send('Successful GET request returning data on all movies');
+  topMovies.find(title)
+  .then((movies) => {
+    res.status(201).json(movies);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error: ' + error)
+  });
 });
 
 app.get('/movieinfo', (req,res) => {
@@ -33,12 +44,56 @@ app.get('/movieinfo/director', (req, res) => {
   res.send('Successful Get request returning movie director information');
 });
 
-app.post('/register', (req, res) => {
-  res.send('Successful POST request in creation of new user');
-});
-
 app.put('/update', (req, res) => {
   res.send('Successful PUT request in updating user info');
+});
+
+app.post('/register', (req, res) => {
+  Users.findOne({ Username: req.body.Username })
+    .then((user) => {
+      if (user) {
+        return res.status(400).send(req.body.Username + 'already exists');
+      } else {
+        Users
+          .create({
+            Username: req.body.Username,
+            Password: req.body.Password,
+            Email: req.body.Email,
+            Birthday: req.body.Birthday
+          })
+          .then((user) =>{res.status(201).json(user) })
+        .catch((error) => {
+          console.error(error);
+          res.status(500).send('Error: ' + error);
+        })
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send('Error: ' + error);
+    });
+});
+
+app.get('/users', (req, res) => {
+  Users.find()
+  .then((users) => {
+    res.status(201).json(users);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error:' + err);
+  });
+});
+
+app.get('/users/:Username', (req, res) => {
+  Users.findOne({ Username: req.params.Username })
+  .then((user) => {
+    res.json(user);
+  })
+  .catch((err) => {
+    console.error(err);
+    res.status(500).send('Error:' + err);
+  });
 });
 
 app.get('/favorites', (req, res) => {
--- movie_list.js
+++ movie_list.js
@@ -12,3 +12,426 @@
   ];
 
 module.exports = topMovies;
+
+/*
+var movie2 = {
+  Title: "The Last: Naruto the Movie",
+  Description: "Hinata Hyuga's younger sister has been kidnapped, so Naruto must do what he can to save her.",
+  Genre: {
+    Name: "Animated",
+    Description: "When images move"
+  },
+  Director: {
+    Name: "Tsuneo Kobayashi",
+    Bio: "Tsuneo Kobayashi was a Japanese anime director affiliated with Pierrot. He was best known as the director of critically acclaimed anime series The Twelve Kingdoms",
+    Birth: "1965-05-05",
+    Death: "Alive"
+  },
+  ImagePath: "the_last.png",
+  Featured: false
+}
+
+db.movies.insertOne(movie2)
+
+var movie3 = {
+  Title: "Schindler/'s List",
+  Description: "In German-occupied Poland during World War II, industrialist Oskar Schindler gradually becomes concerned for his Jewish workforce after witnessing their persecution by the Nazis.",
+  Genre: {
+    Name: "Drama",
+    Description: "When thing get dramatic"
+  },
+  Director: {
+    Name: "Steven Spielberg",
+    Bio: "Steven Allan Spielberg is an American film director, producer, and screenwriter. He began his career in the New Hollywood era, and is currently the most commercially successful director",
+    Birth: "1946-12-18",
+    Death: "Alive"
+  },
+  ImagePath: "schindler.jpg",
+  Featured: true
+}
+
+db.movies.insertOne(movie3)
+
+var movie4 = {
+  Title: "The Conjuring",
+  Description: "Paranormal investigators Ed and Lorraine Warren work to help a family terrorized by a dark presence in their farmhouse.",
+  Genre: {
+    Name: "Horror",
+    Description: "When things are scary"
+  },
+  Director: {
+    Name: "James Wan",
+    Bio: "James Wan is an Australian film director, screenwriter, producer, and comic book writer. He has primarily worked in the horror genre as the co-creator of the Saw and Insidious franchises and the creator of The Conjuring Universe.",
+    Birth: "1977-02-26",
+    Death: "Alive"
+  },
+  ImagePath: "conjuring.jpg",
+  Featured: false
+}
+
+db.movies.insertOne(movie4)
+
+var movie5 = {
+  Title: "Insidious 1",
+  Description: "A family looks to prevent evil spirits from trapping their comatose child in a realm called The Further.",
+  Genre: {
+    Name: "Horror",
+    Description: "When things are scary"
+  },
+  Director: {
+    Name: "James Wan",
+    Bio: "James Wan is an Australian film director, screenwriter, producer, and comic book writer. He has primarily worked in the horror genre as the co-creator of the Saw and Insidious franchises and the creator of The Conjuring Universe.",
+    Birth: "1977-02-26",
+    Death: "Alive"
+  },
+  ImagePath: "insidious.jpg",
+  Featured: false
+}
+
+db.movies.insertOne(movie5)
+
+var movie6 = {
+  Title: "Lights Out",
+  Description: "Rebecca must unlock the terror behind her little brother's experiences that once tested her sanity, bringing her face to face with a supernatural spirit attached to their mother.",
+  Genre: {
+    Name: "Horror",
+    Description: "When things are scary"
+  },
+  Director: {
+    Name: "David F. Sandberg",
+    Bio: "David F. Sandberg is a Swedish filmmaker. He is best known for his collective no-budget horror short films under the online pseudonym ponysmasher and for his 2016 directorial debut Lights Out, based on his 2013 acclaimed horror short of the same name.",
+    Birth: "1981-01-21",
+    Death: "Alive"
+  },
+  ImagePath: "lights_out.jpg",
+  Featured: false
+}
+
+db.movies.insertOne(movie6)
+
+var movie7 = {
+  Title: "Hereditary",
+  Description: "A grieving family is haunted by tragic and disturbing occurrences.",
+  Genre: {
+    Name: "Horror",
+    Description: "When things are scary"
+  },
+  Director: {
+    Name: "Ari Aster",
+    Bio: "Ari Aster is an American film director and screenwriter known for Hereditary and Midsommar.",
+    Birth: "1986-07-15",
+    Death: "Alive"
+  },
+  ImagePath: "hereditary.jpg",
+  Featured: false
+}
+
+db.movies.insertOne(movie7)
+
+var movie8 = {
+  Title: "#Home",
+  Description: "Oliver Twist (Indrans) wants to be tech-savvy and become a better companion to his two sons who spend more time on their phones than with their loved ones.",
+  Genre: {
+    Name: "Drama",
+    Description: "When things are dramatic"
+  },
+  Director: {
+    Name: "Rojin Thomas",
+    Bio: "Rojin Thomas is an Indian film director and script writer who works in Malayalam films. He made his directional debut with Philips and the Monkey Pen (2013), directed along with Shanil Muhammed. His second film was Jo and the Boy (2015). Philips and the Monkey Pen won three awards at 2013 Kerala State Film Awards for Best Children/'s Film, Best Child Artist (Master Sanoop) and Best Children/'s Film Director.",
+    Birth: "1992-09-15",
+    Death: "Alive"
+  },
+  ImagePath: "home.jpg",
+  Featured: false
+}
+
+db.movies.insertOne(movie8)
+
+var movie9 = {
+  Title: "Scary Stories to Tell in the Dark",
+  Description: "On Halloween 1968, Stella and her two friends meet a mysterious drifter, Ramón, and uncover a sinister notebook of stories.",
+  Genre: {
+    Name: "Horror",
+    Description: "When things are scary"
+  },
+  Director: {
+    Name: "André Øvredal",
+    Bio: "He is best known for writing and directing the film Trollhunter",
+    Birth: "1973-05-06",
+    Death: "Alive"
+  },
+  ImagePath: "scary_story.jpg",
+  Featured: false
+}
+
+db.movies.insertOne(movie9)
+
+var movie10 = {
+  Title: "Blair Witch",
+  Description: "After discovering a video showing what he believes to be his vanished sister Heather, James and a group of friends head to the forest believed to be inhabited by the Blair Witch",
+  Genre: {
+    Name: "Horror",
+    Description: "When things are scary"
+  },
+  Director: {
+    Name: "Adam Wingard",
+    Bio: "At 19 Adam got his start in feature film making early with his directorial debut Home Sick, a slasher horror film starring Bill Moseley and Tom Toweles. However it was his second effort at 24 years old with the film Pop Skull that garnered him a talent to watch",
+    Birth: "1982-12-03",
+    Death: "Alive"
+  },
+  ImagePath: "blair_witch.jpg",
+  Featured: false
+}
+
+db.movies.insertOne(movie10)
+
+
+var movie3 = {
+  Title: "",
+  Description: "",
+  Genre: {
+    Name: "",
+    Description: ""
+  },
+  Director: {
+    Name: "",
+    Bio: "",
+    Birth: "",
+    Death: "Alive"
+  },
+  ImagePath: "",
+  Featured: false
+}
+
+db.movies.insertOne(movie3)
+
+var user1 = {
+  Name: "Jason",
+  Password:"123qwe",
+  Email: "choojason7@gmail.com",
+  Birthday: "2001-07-23",
+  Favorites: {
+    List1: "Schindler/'s List"
+  }
+}
+db.users.insertOne(user1)
+
+var user2 = {
+  Name: "Ryan",
+  Password:"456rty",
+  Email: "ryryan@gmail.com",
+  Birthday: "1992-09-09",
+  Favorites: {
+    List1: "Lights Out"
+  }
+}
+db.users.insertOne(user2)
+
+var user3 = {
+  Name: "Ben",
+  Password:"789uio",
+  Email: "bensentit@gmail.com",
+  Birthday: "1995-03-14",
+  Favorites: {
+    List1: "The Conjuring"
+  }
+}
+db.users.insertOne(user3)
+
+var user4 = {
+  Name: "Larry",
+  Password:"asd123",
+  Email: "larryiscool@gmail.com",
+  Birthday: "2000-09-30",
+  Favortites: {
+    List1: "#Home"
+  }
+}
+db.users.insertOne(user4)
+
+var user5 = {
+  Name: "Sam",
+  Password:"rty456",
+  Email: "Samiscool@gmail.com",
+  Birthday: "2004-13-05",
+  Favorites: {
+    List1: "Hereditary"
+  }
+}
+db.users.insertOne(user5)
+
+var user1 = {
+  Name: "",
+  Password:"",
+  Email: "",
+  Birthday: ""
+}
+db.users.insertOne(user1)
+
+var director1 = {
+  Name: "",
+  Bio: "",
+  Birth: "",
+  Death: ""
+}
+
+db.director.insertOne(director1)
+
+var director1 = {
+  Name: "Tsuneo Kobayashi",
+  Bio: "Tsuneo Kobayashi was a Japanese anime director affiliated with Pierrot. He was best known as the director of critically acclaimed anime series The Twelve Kingdoms",
+  Birth: "1965-01-01",
+  Death: "2015-05-01"
+}
+
+db.director.insertOne(director1)
+
+var director2 = {
+  Name: "Steven Spielberg",
+  Bio: "Steven Allan Spielberg is an American film director, producer, and screenwriter. He began his career in the New Hollywood era, and is currently the most commercially successful director",
+  Birth: "1946-12-18",
+  Death: "Alive"
+}
+
+db.director.insertOne(director2)
+
+var director3 = {
+  Name: "James Wan",
+  Bio: "James Wan is an Australian film director, screenwriter, producer, and comic book writer. He has primarily worked in the horror genre as the co-creator of the Saw and Insidious franchises and the creator of The Conjuring Universe.",
+  Birth: "1977-02-26",
+  Death: "Alive"
+}
+
+db.director.insertOne(director3)
+
+var director4 = {
+  Name: "David F. Sandberg",
+  Bio: "David F. Sandberg is a Swedish filmmaker. He is best known for his collective no-budget horror short films under the online pseudonym ponysmasher and for his 2016 directorial debut Lights Out, based on his 2013 acclaimed horror short of the same name.",
+  Birth: "1981-01-21",
+  Death: "Alive"
+}
+
+db.director.insertOne(director4)
+
+var director5 = {
+  Name: "Ari Aster",
+  Bio: "Ari Aster is an American film director and screenwriter known for Hereditary and Midsommar.",
+  Birth: "1986-07-15",
+  Death: "Alive"
+}
+
+db.director.insertOne(director5)
+
+var director6 = {
+  Name: "Rojin Thomas",
+  Bio: "Rojin Thomas is an Indian film director and script writer who works in Malayalam films. He made his directional debut with Philips and the Monkey Pen (2013), directed along with Shanil Muhammed. His second film was Jo and the Boy (2015). Philips and the Monkey Pen won three awards at 2013 Kerala State Film Awards for Best Children/'s Film, Best Child Artist (Master Sanoop) and Best Children/'s Film Director.",
+  Birth: "1992-09-15",
+  Death: "Alive"
+}
+
+db.director.insertOne(director6)
+
+var director7 = {
+  Name: "André Øvredal",
+  Bio: "He is best known for writing and directing the film Trollhunter",
+  Birth: "1973-05-06",
+  Death: "Alive"
+}
+
+db.director.insertOne(director7)
+
+var director8 = {
+  Name: "Adam Wingard",
+  Bio: "At 19 Adam got his start in feature film making early with his directorial debut Home Sick, a slasher horror film starring Bill Moseley and Tom Toweles. However it was his second effort at 24 years old with the film Pop Skull that garnered him a talent to watch",
+  Birth: "1982-12-03",
+  Death: "Alive"
+}
+
+db.director.insertOne(director8)
+
+var director9 = {
+  Name: "Jonathan Demme",
+  Bio: "Robert Jonathan Demme was an American director, producer, and screenwriter.",
+  Birth: "1944",
+  Death: "2017"
+}
+
+db.director.insertOne(director9)
+
+var genre1 = {
+  Name: "Thriller",
+  Description: "Thriller film, also known as suspense film or suspense thriller, is a broad film genre that involves excitement and suspense in the audience."
+}
+
+db.genre.insertOne(genre1)
+
+var genre2 = {
+  Name: "Animated",
+  Description: "When images move"
+}
+
+db.genre.insertOne(genre2)
+
+var genre3 = {
+  Name: "Comedy",
+  Description: "Comedy is a genre of film in which the main emphasis is on humor. These films are designed to make the audience laugh through amusement and most often work by exaggerating characteristics for humorous effect."
+}
+
+db.genre.insertOne(genre3)
+
+var genre4 = {
+  Name: "Drama",
+  Description: "When things get dramatic"
+}
+
+db.genre.insertOne(genre4)
+
+var genre5 = {
+  Name: "Horror",
+  Description: "when things are scary"
+}
+
+db.genre.insertOne(genre5)
+
+var genre6 = {
+  Name: "Mystery",
+  Description: "When you don/'t know who did it"
+}
+
+db.genre.insertOne(genre6)
+
+var genre7 = {
+  Name: "Adventure",
+  Description: "When you want to go somewhere"
+}
+
+db.genre.insertOne(genre7)
+
+var genre8 = {
+  Name: "Action",
+  Description: "When people try to hit each other"
+}
+
+db.genre.insertOne(genre8)
+
+var userlist1 = {
+  list1: "Schindler/'s List"
+}
+
+db.userlist.insertOne(userlist1)
+
+var userlist2 = {
+  list1: "Lights Out"
+}
+
+db.userlist.insertOne(userlist2)
+
+var userlist3 = {
+  list1: "The Conjuring"
+}
+
+db.userlist.insertOne(userlist3)
+
+db.users.update({Name: "Sam"},
+  { $push: {Favorites: ObjectId("61452cf3ea6f6db341ac24b3") }})
+  */
