b6d304485d66973bac77821eb442a58cebd6469c
Cho0 <choojason7@gmail.com>
2022-02-22 19:10:38 -0800

    tech fix

--- index.js
+++ index.js
@@ -39,16 +39,18 @@ const cors = require('cors');
 const { off } = require('process');
 let allowedOrigins = ['http://localhost:8000', 'http://testsite.com', 'http://localhost:1234']
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) {
-      let message = "The CORS policy for this application doesn't allow access from origin " + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+
+app.use(cors())
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) {
+//       let message = "The CORS policy for this application doesn't allow access from origin " + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
 
 let auth = require('./auth')(app);
 
